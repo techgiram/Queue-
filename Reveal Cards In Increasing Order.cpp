0c16b90b288cdf83a1ce371bf02ca8f7f23badcb
Tejas Giram <162475058+techgiram@users.noreply.github.com>
2025-01-08 21:58:44 +0530

    Create Reveal Cards In Increasing Order.cpp

---
+++ Reveal Cards In Increasing Order.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(),deck.end());
+        queue<int>q;
+        int n = deck.size();
+
+        for(int i=0; i<n; i++){
+            q.push(i);
+        } 
+
+        vector<int>ans(n,0);
+        int i=0;
+        while(!q.empty()){
+            int k=q.front();
+            ans[k]= deck[i];
+            q.pop();
+            if(!q.empty()){
+                int x=q.front();
+                q.push(x);
+                q.pop();
+            }
+            i++;
+        }
+        return ans;
+    }
+};
